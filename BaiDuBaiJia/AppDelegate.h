b2bb6247829c0cefd0fb08221e0d2712f8675569
liuqingcan <lqc_loadingu@163.com>
2015-04-27 20:19:57 +0800

    Initial Commit

---
+++ BaiDuBaiJia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiDuBaiJia
+//
+//  Created by WayneLiu on 15/4/27.
+//  Copyright (c) 2015年 WayneLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiDuBaiJia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiDuBaiJia
+//
+//  Created by WayneLiu on 15/4/27.
+//  Copyright (c) 2015年 WayneLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
